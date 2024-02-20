76bdd1bc228e66b1f768de967c66fac4324b4112
Paulsams <pasha.samsonov234@gmail.com>
2024-02-21 01:12:04 +0300

    Add dictionary for keywords

--- Compiler/Lexer/Lexer.cs
+++ Compiler/Lexer/Lexer.cs
@@ -1,9 +1,29 @@
-﻿using ObjectLanguage.Compiler.Lexer.Tokens;
+﻿using System.Collections.ObjectModel;
+using ObjectLanguage.Compiler.Lexer.Tokens;
 
 namespace ObjectLanguage.Compiler.Lexer;
 
 public class Lexer
 {
+    private static readonly ReadOnlyDictionary<string, KeywordType> _keywordTypes = new Dictionary<string, KeywordType>
+    {
+        ["class"] = KeywordType.Class,
+        ["extends"] = KeywordType.Extends,
+        ["var"] = KeywordType.Var,
+        ["this"] = KeywordType.This,
+        ["method"] = KeywordType.Method,
+        ["is"] = KeywordType.Is,
+        ["end"] = KeywordType.End,
+        ["return"] = KeywordType.Return,
+        ["while"] = KeywordType.While,
+        ["loop"] = KeywordType.Loop,
+        ["true"] = KeywordType.True,
+        ["false"] = KeywordType.False,
+        ["if"] = KeywordType.If,
+        ["then"] = KeywordType.Then,
+        ["else"] = KeywordType.Else,
+    }.AsReadOnly();
+    
     private readonly Stream _sourceFile;
 
     public Lexer(Stream sourceFile) => _sourceFile = sourceFile;
